e0329922c1fb8fa3855f994922904fa970381e94
dai-a <hxjsr990@yahoo.co.jp>
2020-12-30 13:38:41 +0900

    modify: Erase meaningless processing

--- env.sh
+++ env.sh
@@ -2,6 +2,9 @@ npx create-react-app react-tictoctoe --template typescript
 
 cp ./App.tsx ./react-tictoctoe/src/
 cp ./App.css ./react-tictoctoe/src/
-sed -i -e 's/react-jsx/react/g' ./react-tictoctoe/tsconfig.json
+
+
+#jsx部分でエラーが起きる問題を回避するために、react-jsx を reactへ置換する。
+#sed -i -e 's/react-jsx/react/g' ./react-tictoctoe/tsconfig.json #TODO: 書き換えた後に初期化されるためこの解決策は効果的ではなかった。
 
 
